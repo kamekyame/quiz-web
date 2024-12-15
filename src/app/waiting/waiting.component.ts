4d5a2c9c68f6affb256d4e266fafe5249e0ddc69
haruto17 <proaca157@gmail.com>
2024-12-15 16:10:44 +0900

    👍 待機中画面のコンポーネントを作成

---
+++ src/app/waiting/waiting.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting',
+  imports: [],
+  templateUrl: './waiting.component.html',
+  styleUrl: './waiting.component.scss'
+})
+export class WaitingComponent {
+
+}
