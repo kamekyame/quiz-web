b8b27d4e706bb8c6bb39e5a1de2151c8113e153a
haruto17 <proaca157@gmail.com>
2024-12-15 17:48:20 +0900

    👍 終了画面のコンポーネントを作成

---
+++ src/app/finish/finish.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finish',
+  imports: [],
+  templateUrl: './finish.component.html',
+  styleUrl: './finish.component.scss'
+})
+export class FinishComponent {
+
+}
