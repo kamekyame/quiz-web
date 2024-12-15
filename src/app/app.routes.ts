02f87d181b9907e1706efa6418f1e67dd91d4204
haruto17 <proaca157@gmail.com>
2024-12-15 17:48:59 +0900

    👍 終了画面のルートを追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { SignupComponent } from "./signup/signup.component";
+import { FinishComponent } from "./finish/finish.component";
 
 export const routes: Routes = [
   {
     path: "signup",
     component: SignupComponent,
+  },
+  {
+    path: "finish",
+    component: FinishComponent,
   },
 ];
