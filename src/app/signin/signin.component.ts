150f14e0e2839299f348e6f0f650936f80112f0d
kameyame <kamekyame@outlook.com>
2024-12-17 20:33:09 +0900

    👍 新規登録・ログイン時に UserService を更新するように

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,39 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { ApiService, isApiError } from '../service/api.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-signin',
   imports: [ReactiveFormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrl: './signin.component.scss',
 })
 export class SigninComponent {
   api = inject(ApiService);
+  userService = inject(UserService);
 
-  formData = new FormGroup ({
-    username: new FormControl(""),
-    password: new FormControl(""),
+  formData = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
   });
 
-  result = "";
+  result = '';
 
   submit() {
     console.log(this.formData.value);
     const data = {
-      username: this.formData.value.username ?? "",
-      password: this.formData.value.password ?? "",
+      username: this.formData.value.username ?? '',
+      password: this.formData.value.password ?? '',
     };
 
-    this.api.signIn(data).subscribe(
-      (data) => {
-        if (isApiError(data)) {
-          this.result = 
-            `ログインに失敗しました。${data.error.message} (${data.error.code})`;
-          return ;
-        }
-        this.result = "ログインに成功しました⭐️";
-      },
-    );
+    this.api.signIn(data).subscribe((data) => {
+      if (isApiError(data)) {
+        this.result = `ログインに失敗しました。${data.error.message} (${data.error.code})`;
+        return;
+      }
+      this.result = 'ログインに成功しました⭐️';
+      this.userService.update();
+    });
   }
 }
