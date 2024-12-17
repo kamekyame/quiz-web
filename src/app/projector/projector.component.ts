6a401b080d17cf7e45adadec39d941f99f818858
haruto17 <proaca157@gmail.com>
2024-12-17 19:27:38 +0900

    👍 プロジェクターに表示する画面のコンポーネントを作成

---
+++ src/app/projector/projector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projector',
+  imports: [],
+  templateUrl: './projector.component.html',
+  styleUrl: './projector.component.scss'
+})
+export class ProjectorComponent {
+
+}
