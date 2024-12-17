1f73f94579b70b3030b74a4c3472f825835f283b
kameyame <kamekyame@outlook.com>
2024-12-17 20:33:45 +0900

    👍 HeaderComponent を追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
-  selector: "app-root",
-  imports: [RouterOutlet, ReactiveFormsModule, RouterLink],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  selector: 'app-root',
+  imports: [RouterOutlet, ReactiveFormsModule, RouterLink, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
