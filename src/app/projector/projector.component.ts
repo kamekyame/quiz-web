a88a30cd7a44350369e48e040acde9cd8e80e36e
haruto17 <proaca157@gmail.com>
2024-12-20 19:39:45 +0900

    👍 プロジェクター用画面でステータスのポーリング処理を追加

--- src/app/projector/projector.component.ts
+++ src/app/projector/projector.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { ApiService, isApiError } from '../service/api.service';
+import { Status } from '../service/api.interface';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-projector',
@@ -6,4 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './projector.component.html',
   styleUrl: './projector.component.scss',
 })
-export class ProjectorComponent {}
+export class ProjectorComponent {
+  api = inject(ApiService);
+
+  pollingSubscription: Subscription | undefined;
+
+  status = signal<Status>(
+    { status: 'waiting' },
+    { equal: (a, b) => JSON.stringify(a) === JSON.stringify(b) },
+  );
+
+  ngOnInit() {
+    // ポーリングでステータスを取得
+    this.pollingSubscription = interval(3000).subscribe(() => {
+      this.updateStatus();
+    });
+  }
+
+  ngOnDestroy() {
+    this.pollingSubscription?.unsubscribe();
+  }
+
+  updateStatus() {
+    this.api.getStatus().subscribe((data) => {
+      if (isApiError(data)) return;
+      this.status.set(data);
+      if (data.status === 'finish') {
+        this.pollingSubscription?.unsubscribe();
+      }
+    });
+  }
+}
