d425e726d1c0af1a2928101a756ca1984acb465f
haruto17 <proaca157@gmail.com>
2024-12-22 21:39:31 +0900

    👍 問題の取得と画面へ表示

--- src/app/projector/screen-open/screen-open.component.ts
+++ src/app/projector/screen-open/screen-open.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
+import { ApiService, isApiError } from '../../service/api.service';
+import {
+  GetQuestionForProjectorRes,
+  Status,
+} from '../../service/api.interface';
 
 @Component({
   selector: 'app-screen-open',
   imports: [],
   templateUrl: './screen-open.component.html',
-  styleUrl: './screen-open.component.scss'
+  styleUrl: './screen-open.component.scss',
 })
 export class ScreenOpenComponent {
+  api = inject(ApiService);
 
+  nowStatus = input.required<Status>();
+
+  questionId = computed(() => {
+    const s = this.nowStatus();
+    if (s.status === 'open' || s.status === 'close') {
+      return s.questionId;
+    } else {
+      return undefined;
+    }
+  });
+
+  question = signal<GetQuestionForProjectorRes | undefined>(undefined);
+
+  result: string | undefined;
+
+  constructor() {
+    effect(() => {
+      const id = this.questionId();
+      if (id === undefined) return;
+      this.api.getQuestionForProjector(id).subscribe((data) => {
+        if (isApiError(data)) {
+          this.result = `${data.error.message} (${data.error.code})`;
+          return;
+        }
+        this.question.set(data);
+      });
+    });
+  }
 }
