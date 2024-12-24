e107764516325ad16420c7b92ae90608e98f0173
haruto17 <proaca157@gmail.com>
2024-12-24 23:32:14 +0900

    👍 リセットボタンをクリックした時の処理を実装

--- src/app/control/delete/delete.component.ts
+++ src/app/control/delete/delete.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ApiService, isApiError } from '../../service/api.service';
 
 @Component({
   selector: 'app-delete',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './delete.component.html',
   styleUrl: './delete.component.scss',
 })
-export class DeleteComponent {}
+export class DeleteComponent {
+  api = inject(ApiService);
+
+  result: string | undefined;
+
+  deleteAnswers() {
+    if (!window.confirm('全ての回答状況をリセットしますか？')) return;
+    if (!window.confirm('この操作は取り消せません。本当によろしいですか？'))
+      return;
+
+    this.api.deleteAnswer().subscribe((data) => {
+      if (isApiError(data)) {
+        this.result = `${data.error.message} (${data.error.code})`;
+        return;
+      }
+      this.result = 'すべての回答状況をリセットしました';
+    });
+  }
+}
