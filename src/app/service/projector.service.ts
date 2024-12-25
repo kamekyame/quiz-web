a9a187bed5f78d43bde1ed64cd44b58439bd9b49
kamekyame <kamekyame@outlook.com>
2024-12-25 23:53:04 +0900

    👍 ProjectorService を追加

---
+++ src/app/service/projector.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { fromEvent, mergeMap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectorService {
+  private targetWindow: Window | null = null;
+
+  setWindow(window: Window | null) {
+    this.targetWindow = window;
+  }
+
+  postMessage(message: string) {
+    if (this.targetWindow === null) return;
+    this.targetWindow.postMessage(message, window.location.origin);
+  }
+
+  messageEvent() {
+    return fromEvent<MessageEvent>(window, 'message').pipe(
+      mergeMap((event) => {
+        if (event.origin !== window.location.origin) return [];
+        if (typeof event.data !== 'string') return [];
+        return [event.data];
+      }),
+    );
+  }
+}
