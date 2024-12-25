9649166852c3a690d4d2ebae3e5bf6b40deb12c6
kamekyame <kamekyame@outlook.com>
2024-12-26 00:32:28 +0900

    🐛 img タグのURLが変わった際に、一度画像をリセットするように変更

--- src/app/directive/auth-image.directive.ts
+++ src/app/directive/auth-image.directive.ts
@@ -22,6 +22,10 @@ export class AuthImageDirective {
       if (!url.includes(ENV.API_BASE)) {
         this.el.nativeElement.src = url;
         return;
+      } else {
+        // 読み込みまでの間は白単色のダミー画像を表示（でないと blokenicon が出てしまう）
+        this.el.nativeElement.src =
+          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2P4////fwAJ+wP9BUNFygAAAABJRU5ErkJggg==';
       }
       this.apiService.getImage(url).subscribe((res) => {
         this.el.nativeElement.src = URL.createObjectURL(res);
